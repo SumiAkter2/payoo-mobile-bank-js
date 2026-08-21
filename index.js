22dbcf301f34b631f5bde6bc4b73ac2865cc416b
SumiAkter2 <akteralisha36@gmail.com>
2026-08-21 09:18:38 +0600

    implement login activity and locate it in home page

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("from index.js");
+document.getElementById("login-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("from index.js");
+  const number = 12345678910;
+  const pin = 1234;
+
+  const getNumber = document.getElementById("number").value;
+  const numberConverted = parseInt(getNumber);
+  const getPin = document.getElementById("pin").value;
+  const pinConverted = parseInt(getPin);
+
+  if (number === numberConverted && pin === pinConverted) {
+    // console.log("yes match your one input");
+    window.location.href = "./home.html";
+  } else {
+    alert("sorry not match either");
+  }
+});
